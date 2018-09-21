9b6d710bbbbb9b00ec4c4c545bdd1566defb373f
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-09-21 09:44:39 -0500

    changes in lock file

---
+++ Assignment1Gurpahul/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1Gurpahul
+{
+    public class Car
+    {
+        public int Cost { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public int Modal { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Assignment1Gurpahul/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1Gurpahul.Startup))]
+namespace Assignment1Gurpahul
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
